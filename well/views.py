14d27402005990c8424629816f9584326839f8ca
EtienneAbeng <abengetienne@gmail.com>
2024-07-01 05:01:50 +0200

    Add login functionality for authentification with password, username.

--- well/views.py
+++ well/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+def home(request):
+    """View function for rendering the home page."""
+    return render(request, 'home.html')
+
+def login_view(request):
+    """View function for handling user login."""
+    if request.method == 'POST':
+        # Extract username and password from POST data
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # If user is authenticated, log them in and redirect to home
+            login(request, user)
+            return redirect('home')
+        else:
+            # Handle authentication failure
+            error_message = "Invalid username or password. Please try again."
+            return render(request, 'login.html', {'error_message': error_message})
+    # Render the login form
+    return render(request, 'login.html')
