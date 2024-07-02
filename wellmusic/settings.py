14d4893f9a7736f6d027674066d3b46ea76de30b
EtienneAbeng <abengetienne@gmail.com>
2024-07-02 12:57:00 +0200

    Refactor settings.py to add MEDIA_ROOT and MEDIA_URL The code changes in `wellmusic/settings.py` add the `MEDIA_ROOT` and `MEDIA_URL` settings to configure the media file storage and URL. This allows the application to handle media files, such as uploaded songs, and serve them correctly to users.

--- wellmusic/settings.py
+++ wellmusic/settings.py
@@ -114,3 +114,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'abengetiennne@gmail.com'
 EMAIL_HOST_PASSWORD = 'Soundcloud1?'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
