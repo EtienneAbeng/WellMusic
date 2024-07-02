27836d2666a30c780c4dd360dbc8287c7097af36
EtienneAbeng <abengetienne@gmail.com>
2024-07-02 15:32:52 +0200

    Refactor well/urls.py to add upload path

--- well/urls.py
+++ well/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),     # Login page
     path('logout/', views.logout_view, name='logout'),  # Logout page
     path('signup/', views.signup_view, name='signup'),
-    path('player/', views.player, name='player'),  # Signup page
+    path('player/', views.player, name='player'),  
     path('search/', views.search, name='search'),
+    path('upload/', views.upload_song, name='upload'),
 
 ]
