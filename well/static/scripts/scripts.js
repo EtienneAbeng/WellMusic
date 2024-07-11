4add1b927f4f96fd19dc9e1bd2eb0a13585ccc80
EtienneAbeng <abengetienne@gmail.com>
2024-07-11 07:38:34 +0200

    Refactor audio control functions in scripts.js

--- well/static/scripts/scripts.js
+++ well/static/scripts/scripts.js
@@ -10,12 +10,12 @@ function pauseAudio(songId) {
 
 function fastForwardAudio(songId) {
   var audio = document.getElementById('audio-' + songId);
-  audio.currentTime += 10; // Fast forward by 10 seconds
+  audio.currentTime += 10;
 }
 
 function rewindAudio(songId) {
   var audio = document.getElementById('audio-' + songId);
-  audio.currentTime -= 10; // Rewind by 10 seconds
+  audio.currentTime -= 10;
 }
 
 function stopAudio(songId) {
@@ -23,6 +23,21 @@ function stopAudio(songId) {
   audio.pause();
   audio.currentTime = 0;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  var songItems = document.querySelectorAll('.song-item');
+  for (var i = 0; i < songItems.length; i++) {
+    var songItem = songItems[i];
+    songItem.addEventListener('click', function() {
+      var audio = this.querySelector('audio');
+      if (audio.paused) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
+    });
+  }
+});
 
 const audioPlayer = document.querySelector('#audio-player');
 const playBtn = document.querySelector('.play-btn');
